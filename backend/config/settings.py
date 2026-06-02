ef0664bc92a9d1adca3d63607f29221a0cf93fd5
Jkris0917 <gelladojohnkris@gmail.com>
2026-06-02 09:34:16 +0800

    chore: Change the mailto into gmail

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -200,3 +200,13 @@ if os.getenv('CLOUDINARY_CLOUD_NAME'):
             'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
         },
     }
+    
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+CONTACT_NOTIFICATION_EMAIL = os.getenv('EMAIL_HOST_USER')
